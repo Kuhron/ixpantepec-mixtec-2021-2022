f17b03bd2b51f6f722e6c684bdbd0cb54e4bad58
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2021-12-09 07:36:34 +0000

    project

--- EandWProjectFall2021/GraphF0.py
+++ EandWProjectFall2021/GraphF0.py
@@ -48,8 +48,16 @@ class Interval:
         self.whistled = "w" in self.label
         self.nasalized = "n" in self.label
         self.glottalized = "'" in self.label
-        self.toneme_level = "".join(get_tonemes_from_str(self.label))
+        self.toneme_level = self.get_toneme_level()
         self.toneme = ("w" if self.whistled else "") + self.toneme_level
+
+    def get_toneme_level(self):
+        s = "".join(get_tonemes_from_str(self.label))
+        no_nos = {"LL": "L", "MM": "M", "HH": "H"}
+        for no_no, rep in no_nos.items():
+            while no_no in s:
+                s = s.replace(no_no, rep)
+        return s
 
     def add_f0_statistic(self, f0_statistic):
         assert type(f0_statistic) is F0Statistic, type(f0_statistic)
@@ -348,18 +356,38 @@ def get_fstds_from_intervals(intervals, semitones=True):
     return np.array([f0.fstd_hz for interval in intervals for f0 in interval.f0_statistics])
 
 
-def hist_multiple_fmean_sets(intervals_list, labels, colors, bins=100, alpha=0.5, semitones=True, mean_and_std_lines=False):
-    for intervals, label, color in zip(intervals_list, labels, colors):
-        assert type(intervals) is list, type(intervals)
-        fmeans = get_fmeans_from_intervals(intervals, semitones=semitones)
-        defined_fmeans = fmeans[np.isfinite(fmeans)]
-        mean_overall = np.mean(defined_fmeans)
-        std_overall = np.std(defined_fmeans)
-        print(f"intervals of label {label} has mean {mean_overall} and std {std_overall}")
-        weights = np.ones_like(fmeans)/float(len(fmeans))
-        hist = plt.hist(fmeans, bins=bins, color=color, label=label, alpha=alpha, weights=weights)
+def hist_multiple_fmean_sets(intervals_list, labels, colors, bins=100, alpha=0.5, semitones=True, mean_and_std_lines=False, separate_axes=False):
+    fmeans_lists = [get_fmeans_from_intervals(intervals, semitones=semitones) for intervals in intervals_list]
+    hist_multiple_sets(fmeans_lists, labels, colors, bins, alpha, semitones, mean_and_std_lines)
+
+
+def hist_multiple_sets(values_lists, labels, colors=None, bins=100, alpha=0.5, semitones=True, mean_and_std_lines=False, separate_axes=False):
+    if separate_axes:
+        n_plots = len(labels)
+        fig = plt.figure()
+        ax1 = plt.subplot(n_plots, 1, 1)
+        axes = [ax1] + [plt.subplot(n_plots, 1, i+1, sharex=ax1) for i in range(1, n_plots)]
+    if colors is None:
+        colors = [None for x in labels]
+    for i, (values, label, color) in enumerate(zip(values_lists, labels, colors)):
+        assert type(values) is list, type(values)
+        values = np.array(values)
+        defined_values = values[np.isfinite(values)]
+        mean_overall = np.mean(defined_values)
+        std_overall = np.std(defined_values)
+        n_samples = len(defined_values)
+        print(f"set of label {label} has mean {mean_overall} and std {std_overall} (n={n_samples})")
+        weights = np.ones_like(values)/float(len(values))
+
+        if separate_axes:
+            ax = axes[i]
+        else:
+            ax = plt.gca()
+        hist = ax.hist(values, bins=bins, color=color, label=label, alpha=alpha, weights=weights)
         if mean_and_std_lines:
-            add_mean_and_std_lines(plt.gca(), fmeans, hist)
+            add_mean_and_std_lines(ax, values, hist)
+        if separate_axes:
+            ax.legend()  # one for each subplot
     plt.legend()
     plt.show()
 
@@ -409,9 +437,44 @@ def plot_f0_trajectory(ts, fmeans_st, fmins_st, fmaxs_st, ranges_st, midpoints_b
     plt.show()
 
 
-def get_stationary_tone_targets(interval):
+def interpolate(ts, ys, interpolation_ts):
+    # assert len(set(ts)) == len(ts), f"duplicate t values: {sorted(t for t in set(ts) if ts.count(t) > 1)}"
+    tups = sorted(zip(ts, ys))
+    ts = [tup[0] for tup in tups]
+    ys = [tup[1] for tup in tups]
+    interpolated_ys = []
+    for interp_t in interpolation_ts:
+        if interp_t in ts:
+            # we already have a value here, just get it
+            interp_y = ys[ts.index(interp_t)]  # inefficient if need to optimize something
+        else:
+            below_t = None
+            above_t = None
+            below_y = None
+            above_y = None
+            for t, y in tups:
+                if t < interp_t:
+                    if below_t is None or t > below_t:
+                        below_t = t
+                        below_y = y
+                elif t > interp_t:
+                    if above_t is None or t < above_t:
+                        above_t = t
+                        above_y = y
+                else:
+                    raise RuntimeError("shouldn't happen")
+            alpha = (interp_t - below_t) / (above_t - below_t)
+            dy = above_y - below_y
+            interp_y = below_y + alpha * dy
+            # print(f"t {below_t:.4f} -> {interp_t:.4f} -> {above_t:.4f} => y {below_y:.4f} -> {interp_y:.4f} -> {above_y:.4f}")
+        interpolated_ys.append(interp_y)
+    return interpolated_ys
+
+
+def get_stationary_tone_targets(interval, slope_tolerance, plot=False):
     # get the min, mean, and max F0 (could also have a condition on the std? but min-max range should be fine as first pass)
     # keep the Nones so the arrays align if somehow one of them has some of these frequencies defined but others undefined (shouldn't be possible I hope, but who knows what Praat will output in weird cases)
+    # slope tolerance is max absolute value of first derivative of F0 wrt time (in semitones/second)
     fmeans_st = [f0.fmean_st for f0 in interval.f0_statistics]
     fmins_st = [f0.fmin_st for f0 in interval.f0_statistics]
     fmaxs_st = [f0.fmax_st for f0 in interval.f0_statistics]
@@ -433,20 +496,24 @@ def get_stationary_tone_targets(interval):
     d2f_dt2s = [d2f/dt2 for d2f, dt2 in zip(d2fmeans, dt2s)]
 
     range_tolerance = 0.1
-    first_derivative_tolerance = 5
     # second_derivative_tolerance = 5  # only use this if you want to restrict it to level tones; if want any critical points then don't put conditions on the second derivative
 
-    times_satisfying_range = [t for t, rng in zip(ts, ranges_st) if abs(rng) <= range_tolerance]
-    times_satisfying_first_derivative = [t for t, df_dt in zip(midpoints_between_ts, df_dts) if abs(df_dt) <= first_derivative_tolerance]
+    # times_satisfying_range = [t for t, rng in zip(ts, ranges_st) if abs(rng) <= range_tolerance]
+    times_satisfying_slope = [t for t, df_dt in zip(midpoints_between_ts, df_dts) if abs(df_dt) <= slope_tolerance]
     # times_satisfying_second_derivative = [t for t, d2f_dt2 in zip(second_order_midpoints, d2f_dt2s) if abs(d2f_dt2) <= second_derivative_tolerance]
 
-    print("times with small range:", times_satisfying_range)
-    print("times with small slope:", times_satisfying_first_derivative)
-    # print(times_satisfying_second_derivative)
-
-    plot_f0_trajectory(ts, fmeans_st, fmins_st, fmaxs_st, ranges_st, midpoints_between_ts, df_dts, second_order_midpoints, d2f_dt2s)
-
     # linearly interpolate the semitones at the desired times
+    # fmeans_at_small_range = interpolate(ts, fmeans_st, times_satisfying_range)
+    fmeans_at_small_slope = interpolate(ts, fmeans_st, times_satisfying_slope)
+    # print("times with small range:", times_satisfying_range)
+    # print("fmeans at these times:", fmeans_at_small_range)
+    # print("times with small slope:", times_satisfying_first_derivative)
+    # print("fmeans at these times:", fmeans_at_small_slope)
+
+    if plot:
+        plot_f0_trajectory(ts, fmeans_st, fmins_st, fmaxs_st, ranges_st, midpoints_between_ts, df_dts, second_order_midpoints, d2f_dt2s)
+
+    return fmeans_at_small_slope
 
 
 if __name__ == "__main__":
@@ -478,56 +545,66 @@ if __name__ == "__main__":
     # plot_hist_fmeans(intervals_by_toneme)
     # plot_fmeans_color_coded(intervals)
 
-    while True:
-        interval = random.choice(intervals)
-        print(interval.label)
-        stationary_targets = get_stationary_tone_targets(interval)
-    # print(stationary_targets)
+    for slope_tolerance in [0.1, 0.316, 1, 3.16, 10]:
+        print(f"slope tolerance is {slope_tolerance}")
+        stationary_targets_by_toneme = {}
+        for interval in intervals:
+            if interval.toneme not in stationary_targets_by_toneme:
+                stationary_targets_by_toneme[interval.toneme] = []
+            stationary_targets_by_toneme[interval.toneme] += get_stationary_tone_targets(interval, slope_tolerance)
+        tonemes_to_plot = ["wL", "wM", "wH"]
+        targets_lists = [stationary_targets_by_toneme[toneme] for toneme in tonemes_to_plot]
+        colors = None
+        hist_multiple_sets(targets_lists, tonemes_to_plot, colors, mean_and_std_lines=True, semitones=True, separate_axes=True)
 
-    # more succinctly writing the plotting code for histograms of tones with certain conditions
 
-    all_tonemes = set(x.toneme for x in intervals)
-    non_low_tonemes = [t for t in all_tonemes if t != "L"]
+    # ----
 
-    intervals_H = get_intervals_by_condition(intervals, tonemes=["H"], whistled=False)
-    intervals_HL = get_intervals_by_condition(intervals, tonemes=["HL"], whistled=False)
-    intervals_HM = get_intervals_by_condition(intervals, tonemes=["HM"], whistled=False)
-    intervals_H_nasal = get_intervals_by_condition(intervals, nasalized=True, tonemes=["H"], whistled=False)
-    intervals_H_not_preceded_by_L = get_intervals_by_condition(intervals, tonemes=["H"], preceding_tonemes=non_low_tonemes, whistled=False)
-    intervals_H_preceded_by_L = get_intervals_by_condition(intervals, tonemes=["H"], preceding_tonemes=["L"], whistled=False)
-    intervals_L = get_intervals_by_condition(intervals, tonemes=["L"], whistled=False)
-    intervals_LH = get_intervals_by_condition(intervals, tonemes=["LH"], whistled=False)
-    intervals_LM = get_intervals_by_condition(intervals, tonemes=["LM"], whistled=False)
-    intervals_M = get_intervals_by_condition(intervals, tonemes=["M"], whistled=False)
-    intervals_MH = get_intervals_by_condition(intervals, tonemes=["MH"], whistled=False)
-    intervals_ML = get_intervals_by_condition(intervals, tonemes=["ML"], whistled=False)
-    intervals_glottalized = get_intervals_by_condition(intervals, glottalized=True, whistled=False)
-    intervals_non_whistled = get_intervals_by_condition(intervals, whistled=False)
-    # caveat: the "not preceded by low" should include the first tone (which isn't preceded by anything)
-    # - but it won't because the condition function assumes any condition on preceding tones disallows the first interval
+    if emma:
+        # more succinctly writing the plotting code for histograms of tones with certain conditions
 
-    # plot the histograms of each of these interval sets
-    intervals_list = [
-        # intervals, 
-        # intervals_non_whistled, 
-        intervals_H_nasal, 
-        intervals_H_preceded_by_L, 
-        intervals_H_not_preceded_by_L, 
-    ]
-    labels = [
-        # "all", 
-        # "non-whistled", 
-        "Hn", 
-        "H / L_", 
-        "H / !L_", 
-    ]
-    colors = [
-        # "blue", 
-        # "blue", 
-        "orange", 
-        "red", 
-        "green", 
-    ]
-    # hist_multiple_fmean_sets(intervals_list, labels, colors, mean_and_std_lines=True, semitones=False)
+        all_tonemes = set(x.toneme for x in intervals)
+        non_low_tonemes = [t for t in all_tonemes if t != "L"]
+
+        intervals_H = get_intervals_by_condition(intervals, tonemes=["H"], whistled=False)
+        intervals_HL = get_intervals_by_condition(intervals, tonemes=["HL"], whistled=False)
+        intervals_HM = get_intervals_by_condition(intervals, tonemes=["HM"], whistled=False)
+        intervals_H_nasal = get_intervals_by_condition(intervals, nasalized=True, tonemes=["H"], whistled=False)
+        intervals_H_not_preceded_by_L = get_intervals_by_condition(intervals, tonemes=["H"], preceding_tonemes=non_low_tonemes, whistled=False)
+        intervals_H_preceded_by_L = get_intervals_by_condition(intervals, tonemes=["H"], preceding_tonemes=["L"], whistled=False)
+        intervals_L = get_intervals_by_condition(intervals, tonemes=["L"], whistled=False)
+        intervals_LH = get_intervals_by_condition(intervals, tonemes=["LH"], whistled=False)
+        intervals_LM = get_intervals_by_condition(intervals, tonemes=["LM"], whistled=False)
+        intervals_M = get_intervals_by_condition(intervals, tonemes=["M"], whistled=False)
+        intervals_MH = get_intervals_by_condition(intervals, tonemes=["MH"], whistled=False)
+        intervals_ML = get_intervals_by_condition(intervals, tonemes=["ML"], whistled=False)
+        intervals_glottalized = get_intervals_by_condition(intervals, glottalized=True, whistled=False)
+        intervals_non_whistled = get_intervals_by_condition(intervals, whistled=False)
+        # caveat: the "not preceded by low" should include the first tone (which isn't preceded by anything)
+        # - but it won't because the condition function assumes any condition on preceding tones disallows the first interval
+
+        # plot the histograms of each of these interval sets
+        intervals_list = [
+            # intervals, 
+            # intervals_non_whistled, 
+            intervals_H_nasal, 
+            intervals_H_preceded_by_L, 
+            intervals_H_not_preceded_by_L, 
+        ]
+        labels = [
+            # "all", 
+            # "non-whistled", 
+            "Hn", 
+            "H / L_", 
+            "H / !L_", 
+        ]
+        colors = [
+            # "blue", 
+            # "blue", 
+            "orange", 
+            "red", 
+            "green", 
+        ]
+        # hist_multiple_fmean_sets(intervals_list, labels, colors, mean_and_std_lines=True, semitones=False)
 
 
